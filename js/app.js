4037ba7e28af46a04fc50f1e503bed535fbec89c
AlexAdutov <alexeyspark@yandex.ru>
2023-09-19 18:12:38 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js'
+
+
+console.log(Game);
+console.log(GameSavingData, 'gmsavingdata');
+console.log(loadGame, 'load');
+console.log(saveGame, 'save');
+
+
+const game = new Game();
+game.start();
